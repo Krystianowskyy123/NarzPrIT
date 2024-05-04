00cb000291bd019d5776421ba307eb1253995fee
Krystianowskyy123 <43277129+Krystianowskyy123@users.noreply.github.com>
2024-05-04 18:37:02 +0200

    projekt13-logs30

--- Laboratorium/Lab04_Git/skrypt.sh
+++ Laboratorium/Lab04_Git/skrypt.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [[ -z "$2" ]]; then
+    num_files=100
+else
+    num_files=$2
+fi
+
 if [[ "$1" == "--date" ]]; then
     date
 elif [[ "$1" == "--logs" ]]; then
-    for ((i=1; i<=100; i++)); do
+    for ((i=1; i<=$num_files; i++)); do
         filename="log${i}.txt"
         echo "Nazwa pliku: $filename" > "$filename"
         echo "Nazwa skryptu: $0" >> "$filename"
         echo "Data: $(date)" >> "$filename"
-    done
+    done 
 else
     echo "Blad"
 fi
